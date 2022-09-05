46fa79322140cac1dec17e7400142dc77503973f
Diana Alejandra <diannalejandra.ps@gmail.com>
2022-09-05 01:04:32 -0500

    U-03 Update Correct error in declare data
    Correct error declaring data changing = to :

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-gray-800" x-data="{ open = false }">
+<nav class="bg-gray-800" x-data="{ open: false }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
             <!-- Mobile menu button-->
@@ -71,7 +71,6 @@
 
                     <!-- Profile dropdown -->
                     <div class="relative ml-3" x-data="{ open: false }">
-                        <div x-data="{ help: false }">
                             <button x-on:click="open=true" type="button"
                                 class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                 id="user-menu-button" aria-expanded="false" aria-haspopup="true">
@@ -82,15 +81,15 @@
                         </div>
 
                         <!--
-                                                                    Dropdown menu, show/hide based on menu state.
+                                                                                Dropdown menu, show/hide based on menu state.
 
-                                                                    Entering: "transition ease-out duration-100"
-                                                                    From: "transform opacity-0 scale-95"
-                                                                    To: "transform opacity-100 scale-100"
-                                                                    Leaving: "transition ease-in duration-75"
-                                                                    From: "transform opacity-100 scale-100"
-                                                                    To: "transform opacity-0 scale-95"
-                                                                -->
+                                                                                Entering: "transition ease-out duration-100"
+                                                                                From: "transform opacity-0 scale-95"
+                                                                                To: "transform opacity-100 scale-100"
+                                                                                Leaving: "transition ease-in duration-75"
+                                                                                From: "transform opacity-100 scale-100"
+                                                                                To: "transform opacity-0 scale-95"
+                                                                            -->
                         <div x-show="open" x-on:click.away="open = false"
                             class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
